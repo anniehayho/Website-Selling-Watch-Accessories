6a4d3d0a8f0444129fae10c1bc506739605b67e5
anniehayho <21522697@gm.uit.edu.vn>
2024-05-04 17:06:15 +0700

    add side bar

---
+++ app/(dashboard)/collections/new/page.tsx
@@ -0,0 +1,8 @@
+import CollectionForm from '@/components/collections/CollectionForm'
+import React from 'react'
+
+export const CreateCollection = () => {
+  return (
+    <CollectionForm />
+  )
+}
